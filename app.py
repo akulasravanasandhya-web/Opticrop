4fed51cf7c36546828650d1917fffafa6cd11611
Akula Sravana sandhya <akulasravanasandhya@gmail.com>
2026-07-20 17:53:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    prediction = "Recommended Crop: Rice"
+
+    return render_template(
+        "index.html",
+        prediction_text=prediction
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
